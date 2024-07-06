892843bb9c0aa8e3808eb72ceda9c6f0ab6e711a
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-07-06 17:03:37 +1000

    Add clsx, twMerge, relocate boilterplate

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="">
+      <ul className="flex flex-row justify-start gap-6">
+        <li>GSAP</li>
+        <li>About</li>
+        <li>Testbed</li>
+      </ul>
+    </header>
+  );
+}
