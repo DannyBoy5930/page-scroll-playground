a739ba8f73d766c713f5a966caba6082e14beb7c
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-07-06 21:59:33 +1000

    Refactor container so that LTBreak is visible without scrolling

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -4,7 +4,7 @@ type ContainerProps = {
 
 export default function Container({ children }: Readonly<ContainerProps>) {
   return (
-    <main className="mx-auto flex min-h-screen max-w-[1050px] flex-1 flex-col px-4 text-black">
+    <main className="mx-auto flex min-h-[98.2vh] max-w-[1050px] flex-1 flex-col px-4 text-black">
       {children}
     </main>
   );
